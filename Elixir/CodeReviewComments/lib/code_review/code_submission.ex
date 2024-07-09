ad9979e277dd77426e437e4672bb4f2b14c55762
MintzyG <ericbraga2000@gmail.com>
2024-07-09 00:30:56 -0300

    Removed unecessary elixir code

--- Elixir/CodeReviewComments/lib/code_review/code_submission.ex
+++
@@ -1,7 +0,0 @@
-defmodule CodeReview.CodeSubmission do
-  @moduledoc """
-  Módulo responsável pela submissão e manipulação de snippets de código.
-  """
-
-  @behaviour CodeReview.CodeSubmissionBehaviour
-end
--- Elixir/CodeReviewComments/test/code_review_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule CodeReviewTest do
-  use ExUnit.Case
-  doctest CodeReview
-
-  test "greets the world" do
-    assert CodeReview.hello() == :world
-  end
-end
